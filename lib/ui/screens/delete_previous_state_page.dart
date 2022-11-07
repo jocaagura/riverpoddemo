b49a287860118b0a11c4f63301867f3f5c322807
Jocaagura <oficinameltablero@gmail.com>
2022-11-07 12:17:59 -0500

    First Lesson and homework ready

--- lib/ui/screens/delete_previous_state_page.dart
+++ lib/ui/screens/delete_previous_state_page.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
-import 'package:riverpoddemo/controllers/counter_controller.dart';
 
+import '../../controllers/counter_controller.dart';
 import 'my_home_page.dart';
 
 class DeletePreviousStatePage extends StatelessWidget {
   const DeletePreviousStatePage({Key? key}) : super(key: key);
+  // TODO: Make sure that the controller used is the same of initial myApp Controller
 
   @override
   Widget build(BuildContext context) {
